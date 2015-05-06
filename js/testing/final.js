8414fed9ae03b52a214d0936767c9a31f2e80c33
icctechnology <spreston@icct.com>
2015-05-06 15:34:48 -0400

    updating git

--- js/testing/final.js
+++ js/testing/final.js
@@ -1,8 +1,10 @@
 //var vizify = new Vizify();
 Vizify.init({library: "google", type: "timeline"});
+
 parse({selector: "table", nameIndex: [0,1], startIndex: 2, endIndex: 3}, function (options) {
     Vizify.load(options)
 });
+
 function parse(options, callback) {
     document.addEventListener("DOMContentLoaded", function () { // ensure page loads so selectors have something
         var selector = options.selector;
@@ -10,14 +12,23 @@ function parse(options, callback) {
         var startIndex = options.startIndex;
         var endIndex = options.endIndex;
         var tables = document.querySelectorAll(selector);
+        var minStartDate = null;
+        var vizifyMinStartDate = document.querySelector(".vizifyMinStartDate");
+        if(vizifyMinStartDate){
+            var startDate = vizifyMinStartDate.innerText.split('\/');
+            minStartDate = new Date(startDate[2], startDate[0], startDate[1]);
+        }
+        var colors = ["#fff", "red", "orange", "blue", "green"];
         for (var t = 0; t < tables.length; t++) {
             var rows = [];
             var table = tables[t];
+            
             if (table.querySelector(".testTable")) {
                 var elt = document.createElement("div");
                 elt.style.width = "60%";
                 elt.style.height = "300px";
                 elt.style.float = "right";
+                elt.style.paddingTop = parseInt(tables[t-1].style.height) + parseInt(tables[t-1].style.top) + 5;
                 elt.id = "vizDiv" + t;
                 insertAfter(table, elt);
                 var trs = table.querySelectorAll("tr");
@@ -25,27 +36,29 @@ function parse(options, callback) {
                     var tds = trs[i].querySelectorAll("td div div span");
                     var title = "";
                     for (var j = 0; j < nameIndex.length; j++) {
-                        title += tds[j].innerText + "-";
-                    }
-                    title.substr(0,title.length -1);
-                    var startDate = tds[startIndex].innerText.split('\/');
-                    var endDate = tds[endIndex].innerText.split('\/');
-                    var row = [
-                        title,
-                        new Date(startDate[2], startDate[0], startDate[1]),
-                        new Date(endDate[2], endDate[0], endDate[1])
-                    ];
-                    rows.push(row);
-                }
-                var data = {
-                    columnName: "test",//trs[0].children[0].innerText,
-                    rows: rows
-                };
+                        title += tds[j].innerText;
+                        if(j < nameIndex.length - 1)
+                           title+= "-";
+                   }
+                   title.substr(0,title.length -1);
+                   var startDate = tds[startIndex].innerText.split('\/');
+                   var endDate = tds[endIndex].innerText.split('\/');
+                   var row = [
+                   title,
+                   new Date(startDate[2], startDate[0], startDate[1]),
+                   new Date(endDate[2], endDate[0], endDate[1])
+                   ];
+                   rows.push(row);
+               }
+               var data = {
+                columnName: trs[0].children[0].innerText,
+                rows: rows
+            };
 
-                callback({data: data, hook: "#vizDiv" + t});
-            }
+            callback({data: data, hook: "#vizDiv" + t, minStartDate: minStartDate, colors: colors});
         }
-    });
+    }
+});
 }
 
 function insertAfter(referenceNode, newNode) {
--- js/testing/vizify.js
+++ js/testing/vizify.js
@@ -9,12 +9,15 @@ var Vizify = {
     hooks: [],
     data: [],
     chartLoader: {},
+    colors: null,
 
     init: function (options) {
         options = options || {};
         this.libType = options.library || "";
         this.chartType = options.type || "";
         this.hook = options.hook || "";
+        this.config = options.config || {};
+        this.minStartDate = options.minStartDate || null;
         if (this.libType === "google") {
             this.loadExt("https://www.google.com/jsapi", this.googleLoadedEvent);
         }
@@ -38,49 +41,77 @@ var Vizify = {
     },
 
     load: function (options) {
+        this.makeSameDayEventTwoDayEvent(options);
+        this.setDataToMinStartDate(options);
+        this.colors = options.colors;
         this.data.push(options.data);
         this.hooks.push(options.hook);
     },
 
-    loadExt: function (url, callback) {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = true;
-        script.src = url;
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
-        script.onload = function () {
+  loadExt: function (url, callback) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = url;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    script.onload = function () {
+        callback();
+    };
+},
+
+loadVisualization: function (callback) {
+    var self = this;
+    google.load("visualization", "1", {
+        callback: function () {
             callback();
-        };
-    },
+        }, packages: [self.chartType]
+    });
+},
 
-    loadVisualization: function (callback) {
-        var self = this;
-        google.load("visualization", "1", {
-            callback: function () {
-                callback();
-            }, packages: [self.chartType]
-        });
-    },
-
-    loadData: function () {
-        if (this.libType === "google" && this.chartType === "timeline") {
-            this.renderGoogleTimeline(this.data, this.hooks);
-        }
-    },
-
-    renderGoogleTimeline: function (d, h) {
-        for (var i = 0; i < d.length; i++) {
-            var data = d[i];
-            var hook = h[i];
-            var container = document.querySelector(hook);
-            var chart = new google.visualization.Timeline(container);
-            var dataTable = new google.visualization.DataTable();
-            dataTable.addColumn({type: 'string', id: data.columnName});
-            dataTable.addColumn({type: 'date', id: 'Start'});
-            dataTable.addColumn({type: 'date', id: 'End'});
-            dataTable.addRows(data.rows);
-            chart.draw(dataTable);
-        }
+loadData: function () {
+    if (this.libType === "google" && this.chartType === "timeline") {
+        this.renderGoogleTimeline(this.data, this.hooks);
     }
+},
+
+renderGoogleTimeline: function (d, h) {
+    for (var i = 0; i < d.length; i++) {
+        var data = d[i];
+        var hook = h[i];
+        var container = document.querySelector(hook);
+        var chart = new google.visualization.Timeline(container);
+        var dataTable = new google.visualization.DataTable();
+        dataTable.addColumn({type: 'string', id: data.columnName});
+        dataTable.addColumn({type: 'date', id: 'Start'});
+        dataTable.addColumn({type: 'date', id: 'End'});
+        dataTable.addRows(data.rows);
+        var options = {};
+        if(this.colors){
+            options= {
+                colors: this.colors
+            };
+        }
+
+        chart.draw(dataTable, options);
+    }
+},
+setDataToMinStartDate: function(options){
+    if(options.minStartDate){
+        var row = [
+        options.data.columnName,
+        options.minStartDate,
+        options.minStartDate
+        ];
+        options.data.rows.unshift(row);
+    }
+},
+
+makeSameDayEventTwoDayEvent: function (options) {
+	for (i = 0; i < options.data.rows.length; i++){
+		if (options.data.rows[i][1].getTime() === options.data.rows[i][2].getTime()) {
+        	options.data.rows[i][2].setDate(options.data.rows[i][2].getDate() + 1);
+    	}
+  	}
+}
 
 };
